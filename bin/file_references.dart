9fc58bfc99234f7096068be5e3052a66ea336b4c
Philip Wintersteiner <philip.wintersteiner@gmx.de>
2024-10-14 16:31:47 +0200

    Add prototype

--- bin/file_references.dart
+++ bin/file_references.dart
@@ -1,1 +1,41 @@
-void main(List<String> arguments) {}
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+void main(List<String> arguments) {
+  const path = '.';
+  final directory = Directory(path);
+  final files = directory.listSync(recursive: true).whereType<File>();
+  final edges = files.map((file) {
+    final fileContents = file.readAsStringSync();
+    return files
+        .map(
+          (otherFile) => fileContents.contains(p.basename(otherFile.path))
+              ? (file, otherFile)
+              : null,
+        )
+        .whereType<(File, File)>();
+  }).expand((i) => i);
+  final dotFile = '''
+digraph FileReferences {
+  # nodes
+${files.map((file) => file.path.quoted).join('\n').indented(2)}
+
+  # edges
+${edges.map(formatDotEdge).join('\n').indented(2)}
+}
+''';
+  print(dotFile);
+}
+
+String formatDotEdge((File, File) edge) =>
+    '${edge.$1.path.quoted} -> ${edge.$2.path.quoted}';
+
+extension StringStuff on String {
+  String get quoted => '"$this"';
+  String indented(final int indentation) =>
+      split('\n').map((line) => repeatString(indentation) + line).join('\n');
+}
+
+String repeatString(int indentation, [String toRepeat = ' ']) =>
+    Iterable.generate(indentation, (_) => toRepeat).join();
